4233a0587f6f5e23c24f2b8222804a81d7ba2883
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2021-05-23 15:10:45 -0400

    repository initial version

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 25f;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    void Update()
+    {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space Key Pressed");
+        }
+        
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4.8f, 2), 0);
+
+        if (transform.position.x >= 11)
+        {
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
+    }
+
+}
